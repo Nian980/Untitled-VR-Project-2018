84b1f639ba50ac6ebbe3ca538e4afbc836a30fe0
Nianxu Wang <nianxuwang@berkeley.edu>
2022-06-17 04:41:18 -0700

    commit

---
+++ story-driven-game/story-driven-game/Assets/_Scenes/TriggerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour {
+
+    public GameObject myCanvas;
+
+    void Start() 
+    {
+    }
+
+    public void triggerPressed() {
+        Debug.Log("Trigger is pressed weee!");
+
+        Debug.Log(myCanvas);
+
+        myCanvas.SetActive(true);
+    }
+
+    // doesn't work
+    //public void triggerClicked() {
+    //    Debug.Log("The trigger is clicked.");
+    //}
+
+    public void triggerReleased() {
+        Debug.Log("Trigger is released! omg!");
+
+        myCanvas.SetActive(false);
+    }
+
+    //left controller?
+    public void triggerPressedLeft() {
+        Debug.Log("Left controller press! Woah!");
+    }
+}
